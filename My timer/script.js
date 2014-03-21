06b49424dfa5c7a927a0c4eab8bae8cab1e9df9d
Rostyk <pas.ros.bor@gmail.com>
2014-03-21 18:49:22 +0200

    2

--- My timer/script.js
+++ My timer/script.js
@@ -66,4 +66,90 @@
 	}
 	window.Timer=Timer;
 	
+	
+	
+	
+	
+	function animateColor(element, start, end, interval)
+	{
+		var start_color = start;
+		var end_color = end;
+		var interval_to_chanje = interval;
+		
+		function colorChange(start_element,end_element,interval)
+		{
+			var frame_counts = Math.floor(interval / 30);
+			
+			return Math.floor((start_element - end_element) / frame_counts);
+		
+		}
+		
+		function run()
+		{
+			function colorToShow()
+				{
+					start_color[0] = start_color[0] - colorChange(start_color[0],end_color[0],interval_to_chanje);
+					start_color[1] = start_color[1] - colorChange(start_color[1],end_color[1],interval_to_chanje);
+					start_color[2] = start_color[2] - colorChange(start_color[2],end_color[2],interval_to_chanje);
+					return start_color;
+					console.log('start_color  ' +start_color);
+				}
+			if(colorToShow()[0]!=end_color[0] && colorToShow()[1]!=end_color[1] && colorToShow()[2]!=end_color[2])
+			{
+				
+				var container = document.getElementById('container');
+
+				var elem = document.createElement(element);
+					
+					elem.style.width = 300;
+					elem.style.height = 200;
+					elem.style.backgroundColor = 'rgb(' + colorToShow()[0] + ',' + colorToShow()[1] + ','+ colorToShow()[2] + ')';
+					
+				container.appendChild(elem);
+				
+			}
+			else
+			{
+				console.log('end');
+			}
+		}
+		
+		run();
+		//setInterval(run,30);
+	}
+	
+	function init()
+	{
+		animateColor('div', [113, 123, 90], [225, 50, 115], 1500);
+	}
+	window.init = init;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }())
